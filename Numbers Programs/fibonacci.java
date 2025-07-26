7a980102cf85a932f2afdab745c8f9be2b5c631d
Shafik03 <shafiknasar@gmail.com>
2025-07-26 15:13:54 +0530

    changed

--- Numbers Programs/fibonacci.java
+++ Numbers Programs/fibonacci.java
@@ -1,4 +1,4 @@
-package java_project;
+
 
 import java.util.Scanner;
 public class fibonacci {
@@ -13,7 +13,7 @@ public class fibonacci {
 	}
 public static void main(String args[]) {
 	Scanner sc= new Scanner(System.in);
-    System.out.print("Enter the size of the series": );
+    System.out.print("Enter the size of the series:" );
 	int N=sc.nextInt();
 	fiboseries(N);
 	sc.close();
