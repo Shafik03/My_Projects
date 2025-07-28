b52d74375853d39ad693f599e75d9299101f7874
Shafik03 <shafiknasar@gmail.com>
2025-07-28 20:53:37 +0530

    correctioned

--- Numbers Programs/prime_num_recursion.java
+++ Numbers Programs/prime_num_recursion.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class prime_num_recursion {
 	public static void main(String args[]) {
 		Scanner sc= new Scanner(System.in);
-        Syatem.out.print("Enter the number: ");
+        System.out.print("Enter the number: ");
 		int n= sc.nextInt();
 		if(isprime(n,2)) {
 		System.out.println(n +" is a prime number");
